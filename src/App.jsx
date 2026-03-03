73ef334f849bed46fab013d2ae07f86fa9811c25
Umar2120 <umarshad121@gmail.com>
2026-03-03 15:04:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -729,7 +729,7 @@ export default function App() {
       <nav className="navbar">
         <div className="logo">
           <div className="logo-icon"><LogoIcon/></div>
-          <span className="logo-text">CINEX</span>
+          <span className="logo-text">MOVIEDEKHO</span>
         </div>
         <div className="nav-cats">
           {NAV_CATEGORIES.map(c => (
